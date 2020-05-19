ffdba420dff69ef522bb7f320cc5385fd8825cec
mononoke <mononoke@themake.rs>
2020-05-20 00:01:51 +0400

    Added styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,9 @@ export default {
     padding 15px
 
 .home__field
-    padding 5px
+    padding 15px
+    font-size 30px
+    text-transform uppercase
+    box-shadow 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)
     cursor pointer
 </style>
